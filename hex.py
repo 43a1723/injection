470c5e13a5e20ceb0f40f5f8eb3bbedde44f267c
43a1723 <hai1723notgay@gmail.com>
2024-06-01 10:15:51 +0700

    Update hex.py

--- hex.py
+++ hex.py
@@ -8,7 +8,7 @@ with open('injection.py', 'rb') as file:
     contentinj = file.read()
 
 hex_content = string_to_hex(content)
-contentinj = contentinj.replace("%code%", hex_content)
+contentinj = contentinj.replace("%code%", hex_content).replace("%hookurl%", "")
 
 # Ghi chuỗi hex vào một tệp mới
 with open('injection-o.py', 'w') as output_file:
