40ae2fd21b9c04b8dbf59deda413d769475fea55
43a1723 <hai1723notgay@gmail.com>
2024-06-01 10:12:11 +0700

    Update hex.py

--- hex.py
+++ hex.py
@@ -1,7 +1,16 @@
-def string_to_hex(string):
-    return ''.join(f'\\x{ord(c):02x}' for c in string)
+def string_to_hex(content):
+    return ''.join(f'\\x{c:02x}' for c in content)
 
 with open('injection.js', 'rb') as file:
     content = file.read()
 
-print(string_to_hex(content))
+with open('injection.py', 'rb') as file:
+    content = file.read()
+
+hex_content = string_to_hex(content)
+
+# Ghi chuỗi hex vào một tệp mới
+with open('injection-o.py', 'w') as output_file:
+    output_file.write(hex_content)
+
+print("Chuỗi hex đã được ghi vào tệp injection-o.js")
