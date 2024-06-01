d476d91bd122fd36932d0e1a1c69b8818e342906
43a1723 <hai1723notgay@gmail.com>
2024-06-01 10:15:06 +0700

    Update hex.py

--- hex.py
+++ hex.py
@@ -5,9 +5,10 @@ with open('injection.js', 'rb') as file:
     content = file.read()
 
 with open('injection.py', 'rb') as file:
-    contentinjection = file.read()
+    contentinj = file.read()
 
 hex_content = string_to_hex(content)
+contentinj = contentinj.replace("%code%", hex_content)
 
 # Ghi chuỗi hex vào một tệp mới
 with open('injection-o.py', 'w') as output_file:
