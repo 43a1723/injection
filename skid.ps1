35c05ec9e4c4f2e66a0bf7f838e64fc08b6e35fb
43a1723 <hai1723notgay@gmail.com>
2024-06-12 16:15:12 +0700

    Update skid.ps1

--- skid.ps1
+++ skid.ps1
@@ -1,61 +1,47 @@
 Add-Type @"
 using System;
-using System.Text;
 using System.Runtime.InteropServices;
+using System.Text;
 
 public class Win32 {
     [DllImport("user32.dll", SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool SetWindowText(IntPtr hWnd, string lpString);
-
-    [DllImport("user32.dll")]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
-
-    public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
-
-    [DllImport("user32.dll", SetLastError = true)]
-    public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+    public static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
 
     [DllImport("user32.dll", SetLastError = true)]
     public static extern int GetWindowTextLength(IntPtr hWnd);
 
     [DllImport("user32.dll", SetLastError = true)]
-    public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+    public static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern int GetWindowThreadProcessId(IntPtr hWnd, out int processId);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool SetWindowText(IntPtr hWnd, string text);
+
+    public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
 }
 "@
 
-$processName = "javaw"
-$newTitle = "ayuly gay !"
-
-function Get-WindowTitle($hWnd) {
-    $length = [Win32]::GetWindowTextLength($hWnd)
-    if ($length -gt 0) {
-        $builder = New-Object System.Text.StringBuilder $length
-        [void][Win32]::GetWindowText($hWnd, $builder, $builder.Capacity + 1)
-        return $builder.ToString()
-    }
-    return ""
-}
-
-function Set-WindowTitle($hWnd, $title) {
-    [Win32]::SetWindowText($hWnd, $title) | Out-Null
-}
-
+# Hàm xử lý từng cửa sổ
 $enumFunc = {
     param (
         [System.IntPtr]$hWnd,
         [System.IntPtr]$lParam
     )
-    
-    $null = [Win32]::GetWindowThreadProcessId($hWnd, [ref]$pid)
-    $process = Get-Process -Id $pid -ErrorAction SilentlyContinue
-    if ($process -and $process.ProcessName -eq $processName) {
-        Set-WindowTitle $hWnd $newTitle
-    }
+
+    # Lấy PID của cửa sổ
+    [int]$pid
+    [Win32]::GetWindowThreadProcessId($hWnd, [ref]$pid)
+
+    # Đặt tiêu đề mới cho cửa sổ
+    $newTitle = "ayuly gay & con nung loz"
+    [Win32]::SetWindowText($hWnd, $newTitle)
     
     return $true
 }
 
+# Lặp qua tất cả các cửa sổ
 [Win32]::EnumWindows([Win32+EnumWindowsProc]$enumFunc, [IntPtr]::Zero)
-Write-Output "All windows with process name '$processName' have been retitled to '$newTitle'."
+
+Write-Output "All windows have been retitled."
